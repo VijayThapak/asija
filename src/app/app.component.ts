43eab8fdfba86b881072fcf399de26af94a0e2b4
Vijay Thapak <vijay.thapak@nagarro.com>
2019-04-12 18:04:39 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hello-world';
+  private firstName = "Vijay";
+  private lastName = "Thapak";
+  bool = false;
+  url = '/a-/AAuE7mDaIPSwLi2eUtSdUZ0Knhmpfg6vTCe09VKiLw=s900-mo-c-c0xffffffff-rj-k-no';
+
+  ngOnInit() {
+    // console.log(this.getName());
+  }
+
+  getName() {
+    return this.firstName + " " + this.lastName;
+    // return `${this.firstName} ${this.lastName}`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Test1Component } from './test1/test1.component';
+import { EmpDetailsComponent } from './emp-details/emp-details.component';
+import { EmpComponent } from './emp/emp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    EmpDetailsComponent,
+    EmpComponent
   ],
   imports: [
     BrowserModule
