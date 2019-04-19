a78d1fa793b557f322af9afd4e76548c58e00101
Vijay Thapak <vijay.thapak@nagarro.com>
2019-04-19 22:14:50 +0530

    19 April clas

---
+++ src/app/19April/events/events.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'cafu-events',
+  templateUrl: './events.component.html',
+  styles: []
+})
+export class EventsComponent implements OnInit {
+  todos = [
+    {todoName: "Todo 1", isCompleted: false},
+    {todoName: "Todo 2", isCompleted: true}
+  ];
+
+  title = 'Events';
+//  @ViewChild('input') inputDom;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick()   {
+    console.log('Hello World..!!', this.title,  this.inputDom);
+   // this.title = "Title Changed..";
+   this.todos.push({isCompleted: false, todoName: this.title});
+
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
 import { EmpDetailsComponent } from './emp-details/emp-details.component';
 import { EmpComponent } from './emp/emp.component';
+import { EventsComponent } from './19April/events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
     EmpDetailsComponent,
-    EmpComponent
+    EmpComponent,
+    EventsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
