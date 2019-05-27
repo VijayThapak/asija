168957aa78a834ca6166385617722d1ed7afd8b1
Vijay Thapak <vijaythapak12@gmail.com>
2019-05-27 23:32:41 +0530

    Reactive Form Example.

--- src/app/14May/reactive-form/reactive-form.component.ts
+++ src/app/14May/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'cafu-reactive-form',
@@ -7,24 +7,62 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styles: []
 })
 export class ReactiveFormComponent implements OnInit {
-  myForm: FormGroup;
+  loginForm: FormGroup;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.initializeForm();
+    // this.initializeForm();
+    this.formGroupType();
   }
 
   initializeForm() {
-    this.myForm = this.fb.group(
-      {
-        uname: ["Vijay"] ,
-        password: ["lksdjfklds"]
-      }
-    );
+    this.loginForm = this.fb.group({
+      userName: [{value: 'Vijay', disabled: false}, [Validators.required]],
+      pwd: ['12345y', [Validators.required, Validators.minLength(5)]],
+      address: this.fb.group({
+        area: [],
+        city: []
+      })
+    });
+  }
+
+  formGroupType() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl("Vijay", [Validators.required]),
+      pwd: new FormControl('345678i', [Validators.minLength(5)]),
+      address: new FormGroup({
+        area: new FormControl('om vihar'),
+        city: new FormControl('Gurgaon')
+      })
+    });
+
+  }
+
+  disableInput(){
+    if(this.loginForm.get('userName').disabled){
+      this.loginForm.get('userName').enable();
+    } else{
+      this.loginForm.get('userName').disable();
+    }
+  }
+
+  resetForm(){
+    //this.loginForm.reset();
+
+    // this.loginForm.setValue({
+    //   userName: "thapak",
+    //   pwd: '2345tyu'
+    // });
+
+    this.loginForm.patchValue({
+      userName: "thapak",
+    });
   }
 
   submitForm(){
-    console.log(this.myForm);
+    // console.log(this.myForm);
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.getRawValue());
   }
 
 }
